f765cd4b20e3c99e998d24ff6193688b1e2be2d2
TrushnaHedaoo <trushhedaoo@gmail.com>
2019-01-29 18:52:42 +0530

    Convert JSON file and XML file To HTML

---
+++ src/XmlToHtml.java
@@ -0,0 +1,53 @@
+
+
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.StringWriter;
+
+import javax.xml.transform.Source;
+import javax.xml.transform.Transformer;
+import javax.xml.transform.TransformerConfigurationException;
+import javax.xml.transform.TransformerException;
+import javax.xml.transform.TransformerFactory;
+import javax.xml.transform.TransformerFactoryConfigurationError;
+import javax.xml.transform.dom.DOMSource;
+import javax.xml.transform.stream.StreamResult;
+import javax.xml.transform.stream.StreamSource;
+
+public class XmlToHtml {
+
+	public static void main(String args[]) {
+		Source xml = new StreamSource(new File("C:\\Users\\HP MY PC\\eclipse-workspace\\XML-JSON-To-HTML\\WebContent\\book.xml"));
+		Source xslt = new StreamSource("C:\\Users\\HP MY PC\\eclipse-workspace\\XML-JSON-To-HTML\\WebContent\\style.xsl");
+
+		convertXMLToHTML(xml, xslt);
+
+	}
+
+	public static void convertXMLToHTML(Source xml, Source xslt) {
+		StringWriter sw = new StringWriter();
+
+		try {
+
+			FileWriter fw = new FileWriter("C:\\Users\\HP MY PC\\eclipse-workspace\\XML-JSON-To-HTML\\WebContent\\books.html");
+			TransformerFactory tFactory = TransformerFactory.newInstance();
+			Transformer trasform = tFactory.newTransformer(xslt);
+			trasform.transform(xml, new StreamResult(sw));
+			fw.write(sw.toString());
+			fw.close();
+
+			System.out
+					.println("book.html generated successfully at c:\\template ");
+
+		} catch (IOException | TransformerConfigurationException e) {
+			e.printStackTrace();
+		} catch (TransformerFactoryConfigurationError e) {
+			e.printStackTrace();
+		} catch (TransformerException e) {
+			e.printStackTrace();
+		}
+
+		
+	}
+}
